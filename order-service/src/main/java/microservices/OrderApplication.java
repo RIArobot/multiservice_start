b6988ced7d0b560c5676db115a5bc7111153819f
Администратор <qqq@sdf.df>
2025-06-16 19:58:47 +0400

    start

--- order-service/src/main/java/microservices/OrderApplication.java
+++ order-service/src/main/java/microservices/OrderApplication.java
@@ -2,9 +2,11 @@ package microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "microservices.client")
 public class OrderApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
